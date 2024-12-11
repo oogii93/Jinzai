9d1a4b2d50a65d1f6a982b0320703b22ef8f4136
nymaa <ba20e065@student.humanities.mn>
2024-12-11 11:34:30 +0900

    asdsdf

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,8 +1,12 @@
-import * as Popper from '@popperjs/core'
-import * as bootstrap from 'bootstrap'
-
+import axios from 'axios';
 import Echo from 'laravel-echo';
 import Pusher from 'pusher-js';
+
+window.axios = axios;
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+
 
 window.Pusher = Pusher;
 
@@ -10,11 +14,8 @@ window.Echo = new Echo({
     broadcaster: 'pusher',
     key: import.meta.env.VITE_PUSHER_APP_KEY,
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-    forceTLS: true,
-    encrypted: true,
-    disableStats: true,
-    wsHost: `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
-    wsPort: 80,
-    wssPort: 443,
-    enabledTransports: ['ws', 'wss']
+    forceTLS: true
 });
+
+
+
